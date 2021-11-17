7fac24e1d2d50f36fb387e0d0b07a48536c78a35
Josh <pianoman04428@gmail.com>
2021-11-17 17:02:44 -0600

    added button layout and update display function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const RUNNING_TOTAL = 0;
+const DISPLAY = document.getElementById("numDisplay");
+const OPERATOR_DISPLAY = document.getElementById
+
 function add(a, b) {
     return a + b;
 }
@@ -13,3 +17,17 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
+
+function clear() {
+    RUNNING_TOTAL = 0;
+}
+
+function updateDisplay() {
+    DISPLAY.textContent = RUNNING_TOTAL
+}
+
+updateDisplay();
