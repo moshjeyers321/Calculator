c7dc265ec1c05e9faf57ddc3170e8a0dcaddc53f
Josh <pianoman04428@gmail.com>
2021-11-18 15:04:05 -0600

    added some button functionality

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
-const RUNNING_TOTAL = 0;
-const DISPLAY = document.getElementById("numDisplay");
-const OPERATOR_DISPLAY = document.getElementById
+const NUM_DISPLAY = document.getElementById("numDisplay");
+const OPERATOR_DISPLAY = document.getElementById("operatorDisplay");
+
+let RUNNING_TOTAL = 0;
+
+const allButtons = document.querySelectorAll("button");
+const functionButtons = document.getElementsByClassName("functionBtn");
+const numberButtons = document.querySelectorAll(".numberBtn");
+
+const clearButton = functionButtons["clear"];
+
+// allButtons.forEach(button => {
+//     button.addEventListener('mouseover', () => {
+//         console.log(button);
+//     })
+// })
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => appendNumber(button.textContent))
+})
+
+clearButton.addEventListener('click', clearScreen)
+
+
+function appendNumber(number) {
+    if (NUM_DISPLAY.textContent === '0') clearNumber();
+    NUM_DISPLAY.textContent += number;
+}
+
+function clearScreen() {
+    NUM_DISPLAY.textContent = '';
+    OPERATOR_DISPLAY.textContent = '';
+}
+
+function clearNumber() {
+    NUM_DISPLAY.textContent = '';
+}
 
 function add(a, b) {
     return a + b;
@@ -22,12 +56,7 @@ function operate(operator, a, b) {
     return operator(a, b);
 }
 
-function clear() {
-    RUNNING_TOTAL = 0;
-}
-
-function updateDisplay() {
-    DISPLAY.textContent = RUNNING_TOTAL
-}
-
-updateDisplay();
+// function updateDisplay() {
+//     NUM_DISPLAY.textContent = DISPLAY_VALUE;
+//     OPERATOR_DISPLAY.textContent = DISPLAY_OPERATOR;
+// }
